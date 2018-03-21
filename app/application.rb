c5e3e193ae6be1affd0b1bc7b3fc03cde776dd57
Sean Oughton <seanoughton@gmail.com>
2018-03-21 18:05:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if time > something 
+    if time > something
       resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
 
     resp.finish
   end
